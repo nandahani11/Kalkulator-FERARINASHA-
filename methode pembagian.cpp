f7a950b446a0ec52c79d0553a7c8a79ed8cab6b8
ShafiraNurRohmah <62553654+ShafiraNurRohmah@users.noreply.github.com>
2020-03-31 13:28:19 +0700

    membuat method pembagian

---
+++ methode pembagian.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void pembagian()
+{
+int x,y;
+    	
+cout << "PEMBAGIAN" << endl;
+cout << "==========" << endl;
+cout << " Masukkan angka : ";
+cin >> x;
+cout << " Masukkan angka : ";
+cin >> y;
+
+cout << " Hasil : " << x / (float)y;
+}
+    
+main()
+{
+pembagian();
+}
