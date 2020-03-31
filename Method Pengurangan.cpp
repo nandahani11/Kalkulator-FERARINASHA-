1a7153718190fcff392d804696d0ecc4f59b35dc
aridimas <62926497+aridimas@users.noreply.github.com>
2020-03-31 13:26:07 +0700

    Add files via upload

---
+++ Method Pengurangan.cpp
@@ -0,0 +1,17 @@
+  
+#include<iostream>
+using namespace std;
+
+void_kurang(){  
+    int a, b;
+    
+    cout<<"Masukkan Bilangan Pertama : ";
+    cin>>a;
+    cout<<"Masukkan Bilangan kedua : ";
+    cin>>b;
+    cout<<"Hasil Pengurangan = "<<a - b;
+}
+
+main(){
+    void_kurang();
+}
